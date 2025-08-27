25d9cab91dadfc6527da34e1108ab5635142f871
natureswaysoil <natureswaysoil@gmail.com>
2025-08-27 16:09:41 -0400

    Add contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <main>
+      <h1>Contact Us</h1>
+      <p>Email: demo@store.com</p>
+      <p>Twitter: @store_demo</p>
+      <p>Feel free to reach out with questions or feedback!</p>
+    </main>
+  );
+}
