e9bdd9c5dc48b990a83e834499386cb01b9881db
natureswaysoil <natureswaysoil@gmail.com>
2025-09-06 03:15:17 +0000

    Add TikTok Pixel via next/script in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
@@ -54,6 +55,17 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       {/* Sends pageview events to Vercel Analytics (cookieless) */}
       <Analytics />
+
+      {/* TikTok Pixel */}
+      <Script id="tiktok-pixel" strategy="afterInteractive">
+        {`
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+  ttq.load('D2TQD1JC77UEJOI3AVC0');
+  ttq.page();
+}(window, document, 'ttq');
+        `}
+      </Script>
     </CartCtxProvider>
   )
 }
