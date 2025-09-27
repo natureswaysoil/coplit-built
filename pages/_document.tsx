538e5f133e0d71d81c9bda8f7fbeb9abc85cbc83
natureswaysoil <natureswaysoil@gmail.com>
2025-09-27 00:28:14 +0000

    Add SEO meta tags and fix accessibility issues

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="BqQM4alYBSVlcmM4Y252DprrpLJiCQ9dc8AiX26N5WA" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
